67666687b55d11728b4644b7f36084dc055686a6
duclh040319 <lehuynhduck4@gmail.com>
2025-08-19 17:16:53 +0700

    replace mongodb url

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongo = () => {
   mongoose
-    .connect("mongodb+srv://duclhgcs220253:<db_password>@bookreview.ebpv3oi.mongodb.net/")
+    .connect("mongodb+srv://duclhgcs220253:j3jYDnxd5EVrzFIf@bookreview.ebpv3oi.mongodb.net/")
     .then(() => {
       console.log(">>> Connected to MongoDB <<<");
     })
